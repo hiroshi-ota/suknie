7e3772d3520248158ba14589edef48979611ce45
hiroshi-ota <kamil.kazmierowski@gmail.com>
2015-04-02 00:14:28 +0200

    poprawione window.onload

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,4 @@
-window.onload = function onLoad() {
+$(document).ready(function onLoad() {
 
 
 	var circle = new ProgressBar.Line
@@ -8,12 +8,14 @@ window.onload = function onLoad() {
 		    easing: 'easeInOut'
 			
 		});
+                
 
-circle.animate(1);
-
-$(document).ready(function(){
-    	$('.loading-mask').delay(1100).fadeOut(1000);
-	$('.progressbar').delay(1000).fadeOut(400);
+$(document).delay(500).ready(circle.animate(1));
 });
 
+window.onload = function(){
+    	$('.loading-mask').delay(1100).fadeOut(1000);
+	$('.progressbar').delay(1000).fadeOut(400);
 };
+
+
