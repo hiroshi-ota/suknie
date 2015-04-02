560bb4df4bc45205c0ff4ec1406229ca62b2496b
kamil <kamil@debian.kamil>
2015-04-02 20:13:57 +0200

    progress bar, rozmiar slider, biblioteki slimbox2 + slimbox na slider

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,19 @@
+window.onload = function onLoad() {
 
+
+	var circle = new ProgressBar.Line
+		('#progress', {
+		    color: '#8E236B',
+		    duration: 1100,
+		    easing: 'easeInOut'
+			
+		});
+
+circle.animate(1);
+
+$(document).ready(function(){
+    	$('.loading-mask').delay(1100).fadeOut(1000);
+	$('.progressbar').delay(1000).fadeOut(400);
+});
+
+};
